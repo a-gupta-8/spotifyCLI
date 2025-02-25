5aa1a32001f971db603d2d4c09e8767e27602cac
a-gupta-8 <avibox9511051@gmail.com>
2025-02-25 14:19:53 -0800

    adding spotify playback controls

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="spotifyCLI",
     version="0.1.0",
     py_modules=["spotifyCLI"],
-    install_requires=["click"],
+    install_requires=["click", "spotipy", "dotenv"],
     entry_points={
         "console_scripts": [
             "spotifycli = spotifyCLI:cli" 
--- spotifyCLI.py
+++ spotifyCLI.py
@@ -1,13 +1,49 @@
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 import click
 
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = 'http://localhost:8888/callback'
+SCOPE = "user-library-read user-read-playback-state user-modify-playback-state"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URI,
+                                               scope=SCOPE))
+
+
+def get_track_uri(song_name, artist_name=None):
+    results = sp.search(q=song_name, type='track', limit=1)  # Get the top result
+    track = results['tracks']['items'][0]
+    track_uri = track['uri']
+    return track_uri
+
+
 @click.command()
-@click.argument("song")
-def play(song):
-    click.echo(f"playing {song}")
+def play():
+    player = sp.current_playback()
+    track = player['item']
+    song_name = track['name']
+    artist = track['artists'][0]['name']
+    click.echo(f"playing {song_name} by {artist}")
+    sp.start_playback()
 
 @click.command()
 def pause():
     click.echo("paused")
+    sp.pause_playback()
+
+@click.command()
+@click.argument("song_name")
+def sing(song_name):
+    track_uri = get_track_uri(song_name)
+    click.echo("Okay.")
+    sp.start_playback(uris=[track_uri])
 
 @click.group()
 def cli():
@@ -15,6 +51,7 @@ def cli():
 
 cli.add_command(play)
 cli.add_command(pause)
+cli.add_command(sing)
 
 if __name__ == "__main__":
     cli()
